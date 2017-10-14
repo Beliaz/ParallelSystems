6890f4d88cf8ae202d708ae34f9c973e4cad56c7
Florian Tischler <florian.tischler@outlook.com>
2017-10-14 13:25:53 +0200

    added script to collect benchmark data

---
+++ Exercise1/collect_benchmark_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv ./build/Testing/Temporary/LastTest.log ./bench/benchmark.lastrun.log
+./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_original.lastrun.json ./bench/mmul_optimized_1.lastrun.json > ./bench/cmp_original_optimization_1.lastrun.txt
+./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_original.lastrun.json ./bench/mmul_optimized_2.lastrun.json > ./bench/cmp_original_optimization_2.lastrun.txt
+./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_optimized_1.lastrun.json ./bench/mmul_optimized_2.lastrun.json > ./bench/cmp_optimization_1_2.lastrun.txt
