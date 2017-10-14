e23d4829ebf4c6ea41ff16ed771838c8ce7414ef
Florian Tischler <florian.tischler@outlook.com>
2017-10-14 17:36:04 +0200

    updated benchmark data collection script to add logs for parallelized implemenation

--- Exercise1/collect_benchmark_data.sh
+++ Exercise1/collect_benchmark_data.sh
@@ -4,3 +4,5 @@ mv ./build/Testing/Temporary/LastTest.log ./bench/benchmark.lastrun.log
 ./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_original.lastrun.json ./bench/mmul_optimized_1.lastrun.json > ./bench/cmp_original_optimization_1.lastrun.txt
 ./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_original.lastrun.json ./bench/mmul_optimized_2.lastrun.json > ./bench/cmp_original_optimization_2.lastrun.txt
 ./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_optimized_1.lastrun.json ./bench/mmul_optimized_2.lastrun.json > ./bench/cmp_optimization_1_2.lastrun.txt
+./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_original.lastrun.json ./bench/mmul_parallelized.lastrun.json > ./bench/cmp_original_parallelized.lastrun.txt
+./build/googlebenchmark-src/tools/compare_bench.py ./bench/mmul_optimized_2.lastrun.json ./bench/mmul_parallelized.lastrun.json > ./bench/cmp_optimized_2_parallelized.lastrun.txt
