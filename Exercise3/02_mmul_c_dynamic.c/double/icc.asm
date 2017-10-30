4d783e6e9990a3cfa050d3c0ba032546bb843512
Ivan Waldboth <Ivan.Waldboth@student.uibk.ac.at>
2017-10-30 18:47:10 +0100

    Update icc.asm

--- Exercise3/02_mmul_c_dynamic.c/double/icc.asm
+++ Exercise3/02_mmul_c_dynamic.c/double/icc.asm
@@ -1,151 +1,81 @@
-..B1.103: # Preds ..B1.101 ..B1.82
-  xor r9d, r9d #53.3
+xor edx, edx #53.3
   test r12d, r12d #53.19
-  jle ..B1.135 # Prob 9% #53.19
-  movsxd r8, r12d #55.7
-  mov r13, QWORD PTR [8+rsp] #55.7[spill]
-..B1.105: # Preds ..B1.133 ..B1.104
-  xor r10d, r10d #54.5
-..B1.106: # Preds ..B1.132 ..B1.105
+  jle ..B1.142 # Prob 9% #53.19
+  mov eax, r12d #55.7
+  and eax, -4 #55.7
+  movsxd rcx, r12d #55.7
+  movsxd rax, eax #55.7
+  mov rdi, QWORD PTR [8+rsp] #55.7[spill]
+..B1.120: # Preds ..B1.140 ..B1.119
+  xor r15d, r15d #54.5
+..B1.121: # Preds ..B1.139 ..B1.120
   test r12d, r12d #55.23
-  jle ..B1.132 # Prob 50% #55.23
-  cmp r12d, 8 #55.7
-  jl ..B1.178 # Prob 10% #55.7
-  mov rsi, QWORD PTR [r14+r9*8] #56.9
-  mov r15, rsi #55.7
-  and r15, 15 #55.7
-  test r15d, r15d #55.7
-  je ..B1.111 # Prob 50% #55.7
-  test r15d, 7 #55.7
-  jne ..B1.178 # Prob 10% #55.7
-  mov r15d, 1 #55.7
-..B1.111: # Preds ..B1.110 ..B1.108
-  lea eax, DWORD PTR [8+r15] #55.7
-  cmp r12d, eax #55.7
-  jl ..B1.178 # Prob 10% #55.7
-  mov r11d, r12d #55.7
-  xor edx, edx #55.7
-  sub r11d, r15d #55.7
-  and r11d, 7 #55.7
-  neg r11d #55.7
-  add r11d, r12d #55.7
-  mov eax, r15d #55.7
-  mov rcx, QWORD PTR [r13+r10*8] #56.30
-  test r15d, r15d #55.7
-  jbe ..B1.177 # Prob 10% #55.7
-  mov rdi, QWORD PTR [rbx+r9*8] #56.20
-  movsd xmm0, QWORD PTR [rdi+r10*8] #56.20
-..B1.114: # Preds ..B1.114 ..B1.113
-  movsd xmm1, QWORD PTR [rcx+rdx*8] #56.30
-  mulsd xmm1, xmm0 #56.30
-  addsd xmm1, QWORD PTR [rsi+rdx*8] #56.9
-  movsd QWORD PTR [rsi+rdx*8], xmm1 #56.9
-  inc rdx #55.7
-  cmp rdx, rax #55.7
-  jb ..B1.114 # Prob 82% #55.7
-..B1.116: # Preds ..B1.114 ..B1.177
-  mov edi, DWORD PTR [r13+r10*8] #56.30
-  movsxd rdx, r11d #55.7
-  lea r15d, DWORD PTR [rdi+r15*8] #56.30
-  test r15d, 15 #55.7
-  je ..B1.120 # Prob 60% #55.7
-  unpcklpd xmm0, xmm0 #56.20
-..B1.118: # Preds ..B1.118 ..B1.117
-  movups xmm1, XMMWORD PTR [rcx+rax*8] #56.30
-  movups xmm3, XMMWORD PTR [16+rcx+rax*8] #56.30
-  movups xmm5, XMMWORD PTR [32+rcx+rax*8] #56.30
-  movups xmm7, XMMWORD PTR [48+rcx+rax*8] #56.30
-  mulpd xmm1, xmm0 #56.30
-  mulpd xmm3, xmm0 #56.30
-  mulpd xmm5, xmm0 #56.30
-  mulpd xmm7, xmm0 #56.30
-  movups xmm2, XMMWORD PTR [rsi+rax*8] #56.9
-  movups xmm4, XMMWORD PTR [16+rsi+rax*8] #56.9
-  movups xmm6, XMMWORD PTR [32+rsi+rax*8] #56.9
-  movups xmm8, XMMWORD PTR [48+rsi+rax*8] #56.9
-  addpd xmm2, xmm1 #56.9
-  addpd xmm4, xmm3 #56.9
-  addpd xmm6, xmm5 #56.9
-  addpd xmm8, xmm7 #56.9
-  movups XMMWORD PTR [rsi+rax*8], xmm2 #56.9
-  movups XMMWORD PTR [16+rsi+rax*8], xmm4 #56.9
-  movups XMMWORD PTR [32+rsi+rax*8], xmm6 #56.9
-  movups XMMWORD PTR [48+rsi+rax*8], xmm8 #56.9
-  add rax, 8 #55.7
-  cmp rax, rdx #55.7
-  jb ..B1.118 # Prob 82% #55.7
-  jmp ..B1.123 # Prob 100% #55.7
-..B1.120: # Preds ..B1.116
-  unpcklpd xmm0, xmm0 #56.20
-..B1.121: # Preds ..B1.121 ..B1.120
-  movups xmm1, XMMWORD PTR [rcx+rax*8] #56.30
-  movups xmm3, XMMWORD PTR [16+rcx+rax*8] #56.30
-  movups xmm5, XMMWORD PTR [32+rcx+rax*8] #56.30
-  movups xmm7, XMMWORD PTR [48+rcx+rax*8] #56.30
-  mulpd xmm1, xmm0 #56.30
-  mulpd xmm3, xmm0 #56.30
-  mulpd xmm5, xmm0 #56.30
-  mulpd xmm7, xmm0 #56.30
-  movups xmm2, XMMWORD PTR [rsi+rax*8] #56.9
-  movups xmm4, XMMWORD PTR [16+rsi+rax*8] #56.9
-  movups xmm6, XMMWORD PTR [32+rsi+rax*8] #56.9
-  movups xmm8, XMMWORD PTR [48+rsi+rax*8] #56.9
-  addpd xmm2, xmm1 #56.9
-  addpd xmm4, xmm3 #56.9
-  addpd xmm6, xmm5 #56.9
-  addpd xmm8, xmm7 #56.9
-  movups XMMWORD PTR [rsi+rax*8], xmm2 #56.9
-  movups XMMWORD PTR [16+rsi+rax*8], xmm4 #56.9
-  movups XMMWORD PTR [32+rsi+rax*8], xmm6 #56.9
-  movups XMMWORD PTR [48+rsi+rax*8], xmm8 #56.9
-  add rax, 8 #55.7
-  cmp rax, rdx #55.7
-  jb ..B1.121 # Prob 82% #55.7
-..B1.123: # Preds ..B1.121 ..B1.118
-  lea eax, DWORD PTR [1+r11] #55.7
-  cmp eax, r12d #55.7
-  ja ..B1.132 # Prob 50% #55.7
-  mov rdi, r8 #55.7
-  sub rdi, rdx #55.7
-  cmp rdi, 2 #55.7
-  jl ..B1.176 # Prob 10% #55.7
-  mov r15d, edi #55.7
-  xor eax, eax #55.7
-  and r15d, -2 #55.7
-  lea rcx, QWORD PTR [rcx+rdx*8] #56.30
-  movsxd r15, r15d #55.7
-  lea rdx, QWORD PTR [rsi+rdx*8] #56.9
-..B1.126: # Preds ..B1.126 ..B1.125
-  movups xmm1, XMMWORD PTR [rcx+rax*8] #56.30
-  movups xmm2, XMMWORD PTR [rdx+rax*8] #56.9
-  mulpd xmm1, xmm0 #56.30
-  addpd xmm2, xmm1 #56.9
-  movups XMMWORD PTR [rdx+rax*8], xmm2 #56.9
-  add rax, 2 #55.7
-  cmp rax, r15 #55.7
-  jb ..B1.126 # Prob 82% #55.7
-..B1.128: # Preds ..B1.126 ..B1.176 ..B1.179
-  cmp r15, rdi #55.7
-  jae ..B1.132 # Prob 10% #55.7
-  movsxd r11, r11d #56.9
-  mov rcx, QWORD PTR [r13+r10*8] #56.30
-  mov rax, QWORD PTR [r14+r9*8] #56.9
-  mov rdx, QWORD PTR [rbx+r9*8] #56.20
-  lea rax, QWORD PTR [rax+r11*8] #56.9
-  movsd xmm0, QWORD PTR [rdx+r10*8] #56.20
-  lea rdx, QWORD PTR [rcx+r11*8] #56.30
+  jle ..B1.139 # Prob 50% #55.23
+  cmp rcx, 4 #55.7
+  jl ..B1.183 # Prob 10% #55.7
+  cmp rcx, 213 #55.7
+  jl ..B1.185 # Prob 10% #55.7
+  mov r11, QWORD PTR [r14+rdx*8] #56.9
+  mov r10, r11 #55.7
+  and r10, 31 #55.7
+  test r10d, r10d #55.7
+  je ..B1.127 # Prob 50% #55.7
+  test r10d, 7 #55.7
+  jne ..B1.183 # Prob 10% #55.7
+  neg r10d #55.7
+  add r10d, 32 #55.7
+  shr r10d, 3 #55.7
+..B1.127: # Preds ..B1.126 ..B1.124
+  mov r9d, r10d #55.7
+  lea rsi, QWORD PTR [4+r9] #55.7
+  cmp rcx, rsi #55.7
+  jl ..B1.183 # Prob 10% #55.7
+  mov r13d, r12d #55.7
+  xor esi, esi #55.7
+  sub r13d, r10d #55.7
+  and r13d, 3 #55.7
+  neg r13d #55.7
+  add r13d, r12d #55.7
+  movsxd r13, r13d #55.7
+  mov r8, QWORD PTR [rdi+r15*8] #56.30
+  test r10d, r10d #55.7
+  jbe ..B1.182 # Prob 10% #55.7
+  mov r10, QWORD PTR [rbx+rdx*8] #56.20
+  vmovsd xmm0, QWORD PTR [r10+r15*8] #56.20
 ..B1.130: # Preds ..B1.130 ..B1.129
-  movsd xmm1, QWORD PTR [rdx+r15*8] #56.30
-  mulsd xmm1, xmm0 #56.30
-  addsd xmm1, QWORD PTR [rax+r15*8] #56.9
-  movsd QWORD PTR [rax+r15*8], xmm1 #56.9
-  inc r15 #55.7
-  cmp r15, rdi #55.7
+  vmovsd xmm1, QWORD PTR [r8+rsi*8] #56.30
+  vfmadd213sd xmm1, xmm0, QWORD PTR [r11+rsi*8] #56.9
+  vmovsd QWORD PTR [r11+rsi*8], xmm1 #56.9
+  inc rsi #55.7
+  cmp rsi, r9 #55.7
   jb ..B1.130 # Prob 82% #55.7
-..B1.132: # Preds ..B1.130 ..B1.123 ..B1.106 ..B1.178 ..B1.128
-  inc r10 #54.5
-  cmp r10, r8 #54.5
-  jb ..B1.106 # Prob 82% #54.5
+..B1.132: # Preds ..B1.130 ..B1.182 ..B1.185
+  vbroadcastsd ymm0, xmm0 #56.20
+..B1.133: # Preds ..B1.133 ..B1.132
+  vmovupd ymm1, YMMWORD PTR [r8+r9*8] #56.30
+  vfmadd213pd ymm1, ymm0, YMMWORD PTR [r11+r9*8] #56.9
+  vmovupd YMMWORD PTR [r11+r9*8], ymm1 #56.9
+  add r9, 4 #55.7
+  cmp r9, r13 #55.7
+  jb ..B1.133 # Prob 82% #55.7
+..B1.135: # Preds ..B1.133 ..B1.183
+  cmp r13, rcx #55.7
+  jae ..B1.139 # Prob 10% #55.7
+  mov rsi, QWORD PTR [rbx+rdx*8] #56.20
+  mov r8, QWORD PTR [r14+rdx*8] #56.9
+  vmovsd xmm0, QWORD PTR [rsi+r15*8] #56.20
+  mov rsi, QWORD PTR [rdi+r15*8] #56.30
+..B1.137: # Preds ..B1.137 ..B1.136
+  vmovsd xmm1, QWORD PTR [rsi+r13*8] #56.30
+  vfmadd213sd xmm1, xmm0, QWORD PTR [r8+r13*8] #56.9
+  vmovsd QWORD PTR [r8+r13*8], xmm1 #56.9
+  inc r13 #55.7
+  cmp r13, rcx #55.7
+  jb ..B1.137 # Prob 82% #55.7
+..B1.139: # Preds ..B1.137 ..B1.135 ..B1.121
+  inc r15 #54.5
+  cmp r15, rcx #54.5
+  jb ..B1.121 # Prob 82% #54.5
   .byte 15 #53.3
   .byte 31 #53.3
   .byte 132 #53.3
@@ -154,6 +84,7 @@
   .byte 0 #53.3
   .byte 0 #53.3
   .byte 0 #53.3
-  inc r9 #53.3
-  cmp r9, r8 #53.3
-  jb ..B1.105 # Prob 82% #53.3
+  inc rdx #53.3
+  cmp rdx, rcx #53.3
+  jb ..B1.120 # Prob 82% #53.3
+  mov r13d, 1 #
