179180612203c3ace2d542312b0449b13e7ac340
Mario Löscher <mario@loescher.tirol>
2017-11-07 13:13:00 +0100

    uploaded a parallel version which even is slower than serial execution using -O3. Unfortunately I did not find the problem, maybe we can discuss that during the PS.

--- Exercise4/test/monteCarlo2Parallel.cpp
+++ Exercise4/test/monteCarlo2Parallel.cpp
@@ -5,29 +5,32 @@
 int throwLoop(int throwCount) {
     double x, y;
 
-    int hitCount = 0;
-    for (int i = 0; i < throwCount; i++) {
-        x = (double) (double) rand() / RAND_MAX;
-        y = (double) (double) rand() / RAND_MAX;
+    int totalHits = 0;
+#pragma omp parallel
+    {
+        int hitCount = 0;
+#pragma omp for schedule(static)
+        for (int i = 0; i < throwCount; i++) {
+            x = (double) (double) rand() / RAND_MAX;
+            y = (double) (double) rand() / RAND_MAX;
 
-        if ((x * x + y * y <= 1))
-            hitCount++;
+            if ((x * x + y * y <= 1))
+                hitCount++;
+        }
+        printf("found %d hits\n", hitCount);
+#pragma omp critical
+        totalHits+=hitCount;
     }
-    printf("found %d hits\n", hitCount);
-    return hitCount;
+    return totalHits;
 }
 
 double getResult(int throwCount, int hitCount) {
     return (double) hitCount / throwCount * 4;
 }
 
-
 int main(void) {
     srand (time(NULL));
-    int throwCount = 1000000;
-//Do parallel stuff here
-
-
+    int throwCount = 10000000;
     int hitCount = throwLoop(throwCount);
     printf("Pi seems to be %f\n", getResult(throwCount, hitCount));
 }
