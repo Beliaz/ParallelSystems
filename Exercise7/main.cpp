2a0f78001e0b4707ea34fe74388a6beb3bbaf3f8
Florian Tischler <florian.tischler@outlook.com>
2017-11-27 15:27:53 +0100

    prettified print for comparison

--- Exercise7/main.cpp
+++ Exercise7/main.cpp
@@ -6,19 +6,17 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
 template<size_t Dim>
 void execute_stencil_code(const float epsilon, 
     const stencil::grid_extents_t<Dim> extents, 
     const bounds_t<Dim> bounds)
 {
-    chrono_timer timer("create (" + std::to_string(Dim) + "D, " +
-        std::to_string(stencil::size(extents)) + ")");
+    using namespace std::chrono;
+    using clock = high_resolution_clock;
 
     auto grid = create_grid<cell_t, Dim>(extents, bounds, {0, 0});
-
-    timer.print();  
-    timer.stop();
 
     constexpr auto printing_threshold = 50;
 
@@ -31,24 +29,34 @@ void execute_stencil_code(const float epsilon,
         std::cout << std::endl;
     }
 
+    std::stringstream ss;
     auto iterations = 0;
 
+    const auto start = clock::now();
+    
+    for(;;)
     {
-        chrono_timer t("jacobi (" + std::to_string(Dim) + "D, " + 
-            std::to_string(stencil::size(extents)) + ")");
+        const auto result = stencil_code::do_iteration(grid, epsilon);
 
-        for(;;)
-        {
-            const auto result = stencil_code::do_iteration(grid, epsilon);
+        iterations += std::get<0>(result);
 
-            iterations += std::get<0>(result);
-
-            if (std::get<1>(result)) break;
-        }
+        if (std::get<1>(result)) break;
     }
 
-    std::cout << "iterations: " << iterations << std::endl;
+    const auto elapsed = duration_cast<milliseconds>(
+        clock::now() - start).count();
 
+    std::cout << elapsed << " ms, " << iterations << " iter";
+
+    if(elapsed > 0)
+    {
+        std::cout << ", " << std::scientific << std::setprecision(3)
+            << iterations * stencil::size(grid.extents()) / static_cast<double>(elapsed) * 1000
+            << " cells/s";
+    }
+
+    std::cout << std::endl;
+    
     if (extents[0] > printing_threshold) return;
 
     // hacky - copies the final values into the first
