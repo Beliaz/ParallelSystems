94ead76e0f74f2383e662df427ba8dd02894d27b
Ivan Waldboth <ivan@waldboth.com>
2017-12-04 12:30:24 +0100

    Task 1

---
+++ Exercise8/stencil_old/requirements.h
@@ -0,0 +1,30 @@
+#ifndef REQUIREMENTS_H
+#define REQUIREMENTS_H
+
+namespace stencil
+{
+    template<class GridType>
+    struct view_provider;
+
+    template<class GridType>
+    typename view_provider<GridType>::first_view_type 
+    get_first(GridType& grid)
+    {
+        return view_provider<GridType>::get_first(grid);
+    }
+
+    template<class GridType>
+    typename view_provider<GridType>::second_view_type 
+    get_second(GridType& grid)
+    {
+        return view_provider<GridType>::get_second(grid);
+    }
+
+    template<class GridType>
+    constexpr auto get_dim()
+    {
+        return view_provider<GridType>::dim;
+    }
+}
+
+#endif // REQUIREMENTS_H
