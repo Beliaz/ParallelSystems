a75f344d3984426b5ddf837cdaec9cc3dcd780fb
Florian Tischler <florian.tischler@outlook.com>
2017-12-11 13:58:04 +0100

    added support for windows (using ms-mpi) added gsl refactoring

---
+++ Exercise9/grid.h
@@ -0,0 +1,194 @@
+//
+// Created by ivan on 06/12/17.
+//
+
+#ifndef PARALLELSYSTEMS_GRID_H
+#define PARALLELSYSTEMS_GRID_H
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <gsl/gsl>
+
+constexpr auto n = 512;
+constexpr auto size = n * n;
+
+
+
+class grid
+{
+public:
+
+    unsigned int xpos;
+    unsigned int ypos;
+    unsigned int from_x;
+    unsigned int to_x;
+    unsigned int from_y;
+    unsigned int to_y;
+    unsigned int blocksize;
+
+
+    void set_from_to(
+        const unsigned int from_x, 
+        const unsigned int to_x, 
+        const unsigned int from_y, 
+        const unsigned int to_y)
+    {
+        this->from_x=from_x;
+        this->to_x=to_x;
+        this->from_y=from_y;
+        this->to_y=to_y;
+        this->blocksize=to_x-from_x;
+    }
+
+    explicit grid(const gsl::span<const double, 4> borders) 
+        :   xpos(0), 
+            ypos(0), 
+            from_x(0), 
+            to_x(0), 
+            from_y(0), 
+            to_y(0), 
+            blocksize(0),
+            data_(size)
+    {
+        for (auto i = 0; i < n; i++)
+        {
+            data_[i] = borders[0];
+            data_[size - 1 - i] = borders[2];
+        }
+
+        for (auto i = 1; i < n - 1; i++)
+        {
+            data_[n - 1 + i * n] = borders[1];
+            data_[i * n] = borders[3];
+        }
+    }
+
+    double get(const unsigned int row, const unsigned int column) const
+    {
+        Expects(row >= 0 && row < n);
+        Expects(column >= 0 && column < n);
+
+        return data_[row * n + column];
+    }
+
+    double get_five(const unsigned int row, const unsigned int column) const
+    {
+        return (get(row,column) +
+                get(row - 1, column) +
+                get(row + 1, column) +
+                get(row, column - 1) +
+                get(row, column +1 ) ) / 5.0;
+    }
+
+    void set(const unsigned int row, const unsigned int column, const double value)
+    {
+        data_[ (row * n) + column ] = value;
+    }
+
+
+    void print() 
+    {
+        for (long i = 0; i < n; i++) 
+        {
+            for (long j = 0; j < n; j++)
+            {
+                std::cout << data_[i * n + j] << " | ";
+            }
+
+            std::cout << std::endl;
+        }
+    }
+
+    std::vector<std::vector<double>> get_block_borders() const
+    {
+        const auto size = to_x - from_x;
+
+        auto borders = std::vector<std::vector<double>>();
+
+        borders.push_back(std::vector<double>(size));
+        borders.push_back(std::vector<double>(size));
+        borders.push_back(std::vector<double>(size));
+        borders.push_back(std::vector<double>(size));
+
+        for (auto i = 0u; i < size; ++i) {
+            borders[0][i] = get(from_x , from_y + i);
+            //      borders[0][size + counter++] = get(from_x + 1, from_y + i);
+        }
+
+        for (auto i = 0u; i < size; ++i) {
+            borders[1][i] = get(from_x + i, to_y);
+            //     borders[1][size + counter++] = get(from_x + i, to_y - 1);
+        }
+
+        for (auto i = 0u; i < size; ++i) {
+            borders[2][i] = get(to_x, from_y + i);
+            //    borders[2][size + counter++] = get(to_x - 1, from_y + i);
+        }
+
+        for (auto i = 0u; i < size; ++i) {
+            borders[3][i] = get(from_x + i, from_y);
+            //   borders[3][size + counter++] = get(from_x + i, from_y + 1);
+        }
+
+        return borders;
+    }
+
+    void set_block_borders(double* borders,
+                           const unsigned direction)
+    {
+        const auto size = to_x - from_x;
+
+        switch (direction)
+        {
+            case 0:
+            {
+                for (auto i = 0u; i < size; ++i)
+                    set(from_x - 1, from_y + i, borders[i]);
+
+            } break;
+
+            case 1:
+            {
+                for (auto i = 0u; i < size; ++i)
+                    set(from_x + i, to_y + 1, borders[i]);
+
+            } break;
+
+            case 2:
+            {
+                for (auto i = 0u; i < size; ++i)
+                    set(to_x + 1, from_y + i, borders[i]);
+
+            } break;
+
+            case 3:
+            {
+                for (auto i = 0u; i < size; ++i)
+                    set(from_x + i, from_y - 1, borders[i]);
+
+            } break;
+
+            default: throw std::logic_error("unexpected direction");
+        }
+    }
+
+    void set_block(const int my_rank, const int blocks)
+    {
+        xpos = my_rank / blocks;
+        ypos = my_rank % blocks;
+
+        const auto elems_per_block = n / blocks;
+
+        set_from_to(
+            xpos * elems_per_block, 
+            (xpos + 1) * elems_per_block, 
+            ypos * elems_per_block, 
+            (ypos + 1) * elems_per_block);
+    }
+
+private:
+    std::vector<double> data_;
+};
+
+#endif //PARALLELSYSTEMS_GRID_H
---
+++ Exercise9/stencil.cpp
@@ -0,0 +1,104 @@
+//
+// Created by ivan on 04/12/17.
+//
+
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+#include <mpi.h>
+#include <cmath>
+#include "grid.h"
+#include "mpi_stencil.h"
+#include <array>
+#include "gsl/gsl"
+
+constexpr auto epsilon = 20;
+
+int main(int argc, char **argv) 
+{
+    const std::array<const double, 4> borders
+    {
+        1.0, 0.5, 0, -0.5
+    };
+
+    auto grid1 = new grid(borders);
+    auto grid2 = new grid(borders);
+
+    unsigned iterations = 0;
+    unsigned sum_iterations;
+    
+    //Start to measure time
+    using clock = std::chrono::high_resolution_clock;
+    const auto start = clock::now();
+
+    MPI_Init(&argc, &argv);
+
+    auto num_procs = -1;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    auto my_rank = -1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    const auto blocks = static_cast<int>(sqrt(num_procs));
+
+    if( size % num_procs != 0 && blocks * blocks != num_procs) 
+    {
+        if (my_rank == 0)
+        {
+            std::cerr << "Matrix size not divisible by number of processors: " 
+                      << num_procs << std::endl;
+        }
+           
+        MPI_Finalize();
+        
+        return EXIT_FAILURE;
+    }
+
+    grid1->set_block(my_rank, blocks);
+    grid2->set_block(my_rank, blocks);
+
+    stencil s(num_procs, my_rank, grid1);
+    
+    // Always do two iterations, as the arrays have to switch every time.
+    // This way, it is not needed to keep track which was the last
+    while (true) 
+    {
+        s.iteration(grid1, grid2);
+        s.send_recv_border(grid2);
+
+        const auto d_epsilon = s.iteration(grid2, grid1);
+        s.send_recv_border(grid1);
+
+        iterations+=2;
+
+        double sum_epsilon;
+        MPI_Allreduce(&d_epsilon, &sum_epsilon, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+        if (sum_epsilon < epsilon) 
+            break;
+    }
+
+    MPI_Reduce(&iterations,&sum_iterations,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
+
+    MPI_Finalize();
+
+    if (my_rank != 0) return EXIT_SUCCESS;
+
+    const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+            clock::now() - start).count();
+
+    std::cout << elapsed << " ms, " << iterations << " iter";
+
+    if(elapsed > 0)
+    {
+        std::cout << ", " << std::scientific << std::setprecision(3)
+                    << iterations * size / static_cast<double>(elapsed) * 1000
+                    << " cells/s";
+    }
+
+    std::cout << std::endl;
+   
+    return EXIT_SUCCESS;
+}
+
